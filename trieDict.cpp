c4853a40c241d18e8c42f6f434ec3f697068ab0b
Furkan Göçük <112954805+linnathoncode@users.noreply.github.com>
2023-11-10 22:21:41 +0300

    Add files via upload

---
+++ trieDict.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#define MAX_CHAR 26
+using namespace std;
+
+class trieNode
+{
+	public:
+		char data;
+		trieNode *children [MAX_CHAR];
+		bool isEndOfWord;
+		
+		trieNode()
+		{
+			data = '\0';
+			isEndOfWord = false;
+			for(int i = 0; i< MAX_CHAR; i++) children[i] = '\0';
+		}
+};
+
+class Trie
+{	
+	public:
+		Trie()
+		{ 
+			root = new trieNode;
+		}
+		void insert(char word[MAX_CHAR]);	
+		bool search(char word[MAX_CHAR]);
+	private:
+		trieNode *root;	
+			
+};
+
+void Trie::insert(char word[MAX_CHAR])
+{
+	trieNode * current = root;
+	
+	for(int i = 0; word[i] != '\0'; i++)
+	{
+		int index = word[i] - 'a';
+		
+		if(!current -> children[index])
+		{
+			current -> children[index]= new trieNode;
+		}
+		current = current-> children[index];
+	}
+	current -> isEndOfWord = true;	
+}
+
+bool Trie::search(char word[MAX_CHAR])
+{
+	int i = 0;
+	trieNode *current = root;
+
+	
+	while(word[i])
+	{	
+		int index = word[i] - 'a';
+		if(!current -> children[index])
+		{
+			return false;
+		}
+		else
+		{
+			current = current ->children[index];
+			i++;
+		}
+		
+	}
+	return current && current -> isEndOfWord; // returns true if only there is character
+												// and its the last character of the given word
+
+}
+
+int main()
+{
+	Trie t;
+	char word[MAX_CHAR];
+	int menu;
+		while(1){
+			cout<<"1.Insert"<<endl;
+			cout<<"2.Search"<<endl;
+			cout<<"7.Exit"<<endl;
+			cout<<"Do your selection:";
+			cin>>menu;
+			
+			switch(menu)
+			{
+				case 1:
+					cout<<"Enter a word to be inserted: ";
+					cin>>word;
+					t.insert(word);
+					break;
+				case 2:
+					cout<<"Enter a word to be searched: ";
+					cin>>word;
+					if(t.search(word)) cout<<word<<" is in the dictionary."<<endl;
+					else cout<<word<<" is not in the dictionary"<<endl;
+					break;
+				case 7:
+					exit(1);
+				default:
+					cout<<"Wrong Selection!"<<endl;	
+			}
+		}
+	}
